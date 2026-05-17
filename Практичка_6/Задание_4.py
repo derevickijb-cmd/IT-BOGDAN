e2a886206da9c6e7fcc9c22eebdc845e821cf97a
Bogdan Derevitskye <derevickijb@gmail.com>
2026-05-17 17:42:56 +0300

    Обновлен код в файле

--- Практичка_6/Задание_4.py
+++ Практичка_6/Задание_4.py
@@ -1,8 +1,8 @@
 cell1, column1 = map(int, input("Введите номер клетки и столбца(от 1 до 8): ").split())
 cell2, column2 = map(int, input("Введите ёше номер клетки и столбца(от 1 до 8): ").split())
+
 if ((cell1 - column1) - (cell2 - column2)) % 2 == 0:
     print ("YES")
-
 elif column1 == column2:
     print ("NO")
 elif cell1 == cell2:
